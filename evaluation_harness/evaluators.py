d1450f2d872ca2c4ab649d2543c6c43a4a7e87ff
alexisxy <alexisxy0418@gmail.com>
2023-09-15 22:28:58 -0400

    update evaluators to match the new config format

--- evaluation_harness/evaluators.py
+++ evaluation_harness/evaluators.py
@@ -10,6 +10,7 @@ from typing import Any, Tuple, Union
 import evaluate  # type: ignore[import]
 from beartype import beartype
 from beartype.door import is_bearable
+from nltk.tokenize import word_tokenize
 from playwright.sync_api import CDPSession, Page
 
 from browser_env.actions import Action
@@ -66,36 +67,6 @@ class Evaluator(object):
 
 
 @beartype
-class StringExactEvaluator(Evaluator):
-    """Check whether the answer is exactly the same as one of the reference answers"""
-
-    def __call__(
-        self,
-        trajectory: Trajectory,
-        config_file: Path | str,
-        page: Page | None = None,
-        client: CDPSession | None = None,
-    ) -> float:
-        with open(config_file, "r") as f:
-            configs = json.load(f)
-
-        def clean_answer(answer: str) -> str:
-            if answer.startswith("'") and answer.endswith("'"):
-                answer = answer[1:-1]
-            elif answer.startswith('"') and answer.endswith('"'):
-                answer = answer[1:-1]
-            return answer
-
-        last_action = self.get_last_action(trajectory)
-        pred = clean_answer(last_action["answer"])
-        ref = [clean_answer(x) for x in configs["eval"]["reference_answers"]]
-        if pred in ref:
-            return 1.0
-        else:
-            return 0.0
-
-
-@beartype
 class StringEvaluator(Evaluator):
     """Check whether the answer is correct with:
     exact match: the answer is exactly the same as the reference answer
@@ -103,6 +74,41 @@ class StringEvaluator(Evaluator):
     fuzzy match: the answer is similar to the reference answer, using LLM judge
     """
 
+    @staticmethod
+    @beartype
+    def clean_answer(answer: str) -> str:
+        if answer.startswith("'") and answer.endswith("'"):
+            answer = answer[1:-1]
+        elif answer.startswith('"') and answer.endswith('"'):
+            answer = answer[1:-1]
+        return answer.lower()
+
+    @staticmethod
+    @beartype
+    def exact_match(ref: str, pred: str) -> float:
+        return float(
+            StringEvaluator.clean_answer(pred)
+            == StringEvaluator.clean_answer(ref)
+        )
+
+    @staticmethod
+    @beartype
+    def must_include(ref: str, pred: str) -> float:
+        clean_ref = StringEvaluator.clean_answer(ref)
+        clean_pred = StringEvaluator.clean_answer(pred)
+        # tokenize the answer if the ref is a single word
+        # prevent false positive (e.g, 0)
+        if len(word_tokenize(clean_ref)) == 1:
+            tok_pred = word_tokenize(clean_pred)
+            return float(clean_ref in tok_pred)
+        else:
+            return float(clean_ref in clean_pred)
+
+    @staticmethod
+    @beartype
+    def fuzzy_match(ref: str, pred: str, intent: str) -> float:
+        return llm_fuzzy_match(pred, ref, intent)
+
     def __call__(
         self,
         trajectory: Trajectory,
@@ -113,34 +119,25 @@ class StringEvaluator(Evaluator):
         with open(config_file, "r") as f:
             configs = json.load(f)
 
-        def clean_answer(answer: str) -> str:
-            if answer.startswith("'") and answer.endswith("'"):
-                answer = answer[1:-1]
-            elif answer.startswith('"') and answer.endswith('"'):
-                answer = answer[1:-1]
-            return answer.lower()
-
         last_action = self.get_last_action(trajectory)
-        pred = clean_answer(last_action["answer"])
+        pred = self.clean_answer(last_action["answer"])
 
         score = 1.0
         for approach, value in configs["eval"]["reference_answers"].items():
             match approach:
                 case "exact_match":
-                    assert isinstance(value, str)
-                    ref_answer = clean_answer(value)
-                    score = score * (pred == ref_answer)
+                    score *= self.exact_match(ref=value, pred=pred)
                 case "must_include":
                     assert isinstance(value, list)
                     for must_value in value:
-                        must_value = clean_answer(must_value)
-                        score = score * (must_value in pred)
+                        score *= self.must_include(ref=must_value, pred=pred)
                 case "fuzzy_match":
                     intent = configs["intent"]
                     assert isinstance(value, list)
                     for reference in value:
-                        fuzzy_score = llm_fuzzy_match(pred, reference, intent)
-                        score = score * fuzzy_score
+                        score *= self.fuzzy_match(
+                            ref=reference, pred=pred, intent=intent
+                        )
         return score
 
 
@@ -216,10 +213,6 @@ class HTMLContentExactEvaluator(Evaluator):
         page: Page,
         client: CDPSession | None = None,
     ) -> float:
-        def clean(text: str) -> str:
-            text = str(text)
-            return text.strip().lower()
-
         with open(config_file, "r") as f:
             configs = json.load(f)
 
@@ -233,9 +226,6 @@ class HTMLContentExactEvaluator(Evaluator):
                 func = func.replace("__last_url__", page.url)
                 target_url = eval(func)
 
-            required_contents: str = target[
-                "required_contents"
-            ]  # what contents to check
             locator: str = target["locator"]  # js element locator
 
             # navigate to that url
@@ -264,16 +254,28 @@ class HTMLContentExactEvaluator(Evaluator):
             else:
                 raise ValueError(f"Unknown locator: {locator}")
 
-            required_contents_or = [
-                clean(x) for x in required_contents.split(" |OR| ")
-            ]
-            selected_element = clean(selected_element)
-            score *= any(
-                [
-                    content in selected_element
-                    for content in required_contents_or
-                ]
-            )
+            if "exact_match" in target["required_contents"]:
+                required_contents = target["required_contents"]["exact_match"]
+                score *= StringEvaluator.exact_match(
+                    ref=required_contents, pred=selected_element
+                )
+            elif "must_include" in target["required_contents"]:
+                required_contents = target["required_contents"]["must_include"]
+                assert isinstance(required_contents, list)
+                for content in required_contents:
+                    content_or = content.split(" |OR| ")
+                    score *= any(
+                        [
+                            StringEvaluator.must_include(
+                                ref=content, pred=selected_element
+                            )
+                            for content in content_or
+                        ]
+                    )
+            else:
+                raise ValueError(
+                    f"Unknown required_contents: {target['required_contents'].keys()}"
+                )
 
         return score
 
