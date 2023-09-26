b4c917dd45825ff33019dd345d95ee5e1bb624c7
alexisxy <alexisxy0418@gmail.com>
2023-09-26 15:27:07 -0400

    update must_include tokenization condition; upate url match

--- evaluation_harness/evaluators.py
+++ evaluation_harness/evaluators.py
@@ -1,5 +1,7 @@
 """base class for evaluation"""
 # answer string match
+import collections
+import html
 import importlib
 import json
 import time
@@ -77,6 +79,7 @@ class StringEvaluator(Evaluator):
     @staticmethod
     @beartype
     def clean_answer(answer: str) -> str:
+        answer = answer.strip()
         if answer.startswith("'") and answer.endswith("'"):
             answer = answer[1:-1]
         elif answer.startswith('"') and answer.endswith('"'):
@@ -93,12 +96,16 @@ class StringEvaluator(Evaluator):
 
     @staticmethod
     @beartype
-    def must_include(ref: str, pred: str) -> float:
+    def must_include(ref: str, pred: str, tokenize=False) -> float:
         clean_ref = StringEvaluator.clean_answer(ref)
         clean_pred = StringEvaluator.clean_answer(pred)
         # tokenize the answer if the ref is a single word
         # prevent false positive (e.g, 0)
-        if len(word_tokenize(clean_ref)) == 1:
+        if (
+            tokenize
+            and len(clean_ref) == 1
+            and len(word_tokenize(clean_ref)) == 1
+        ):
             tok_pred = word_tokenize(clean_pred)
             return float(clean_ref in tok_pred)
         else:
@@ -130,7 +137,11 @@ class StringEvaluator(Evaluator):
                 case "must_include":
                     assert isinstance(value, list)
                     for must_value in value:
-                        score *= self.must_include(ref=must_value, pred=pred)
+                        score *= self.must_include(
+                            ref=must_value,
+                            pred=pred,
+                            tokenize=(len(value) == 1),
+                        )
                 case "fuzzy_match":
                     intent = configs["intent"]
                     assert isinstance(value, list)
@@ -165,7 +176,7 @@ class StringSoftEvaluator(Evaluator):
 
 
 class URLExactEvaluator(Evaluator):
-    """Check whether the URL is exactly the same as of the reference URLs"""
+    """Check URL matching"""
 
     @beartype
     def __call__(
@@ -180,26 +191,59 @@ class URLExactEvaluator(Evaluator):
 
         def clean_url(url: str) -> str:
             url = str(url)
-            if url.endswith("/"):
-                url = url[:-1]
+            url = url.rstrip("/")
             return url
+
+        def parse_url(url: str) -> tuple[str, dict[str, list[str]]]:
+            """Parse a URL into its base, path, and query components."""
+            parsed_url = urllib.parse.urlparse(url)
+            base_path = parsed_url.netloc + parsed_url.path
+            query = urllib.parse.parse_qs(parsed_url.query)
+            return base_path, query
+
+        def parse_urls(
+            urls: list[str],
+        ) -> tuple[list[str], list[str], dict[str, set[str]]]:
+            """Parse a list of URLs."""
+            base_paths = []
+            queries = collections.defaultdict(set)
+            for url in urls:
+                base_path, query = parse_url(url)
+                base_paths.append(base_path)
+                for k, v in query.items():
+                    queries[k].update(v)
+            return base_paths, queries
 
         pred = clean_url(page.url)
         ref_urls = configs["eval"]["reference_url"].split(" |OR| ")
         ref_urls = [clean_url(url) for url in ref_urls]
-        matching_rule = configs["eval"].get("url_note", "EXACT")
-        if matching_rule == "EXACT":
-            if pred in ref_urls:
-                return 1.0
-            else:
-                return 0.0
-        elif matching_rule == "GOLD in PRED":
-            if any([ref in pred for ref in ref_urls]):
-                return 1.0
-            else:
-                return 0.0
+        matching_rule = configs["eval"].get("url_note", "GOLD in PRED")
+        if matching_rule == "GOLD in PRED":
+            ref_base_paths, ref_queries = parse_urls(ref_urls)
+            pred_base_paths, pred_query = parse_url(pred)
+
+            base_score = float(
+                any(
+                    [
+                        ref_base_path in pred_base_paths
+                        for ref_base_path in ref_base_paths
+                    ]
+                )
+            )
+            query_score = 1.0
+            for k, possible_values in ref_queries.items():
+                query_score *= float(
+                    any(
+                        possible_ref_value in pred_query.get(k, [])
+                        for possible_ref_value in possible_values
+                    )
+                )
+            score = base_score * query_score
+
         else:
             raise ValueError(f"Unknown matching rule: {matching_rule}")
+
+        return score
 
 
 class HTMLContentExactEvaluator(Evaluator):
@@ -241,10 +285,9 @@ class HTMLContentExactEvaluator(Evaluator):
                 "[...document."
             ):
                 try:
-                    selected_element = page.evaluate(f"() => {locator}")
+                    selected_element = str(page.evaluate(f"() => {locator}"))
                     if not selected_element:
                         selected_element = ""
-                    selected_element = str(selected_element)
                 except Exception:
                     # the page is wrong, return empty
                     selected_element = ""
@@ -256,29 +299,34 @@ class HTMLContentExactEvaluator(Evaluator):
             else:
                 raise ValueError(f"Unknown locator: {locator}")
 
+            selected_element = html.unescape(selected_element)
+
             if "exact_match" in target["required_contents"]:
                 required_contents = target["required_contents"]["exact_match"]
-                score *= StringEvaluator.exact_match(
+                cur_score = StringEvaluator.exact_match(
                     ref=required_contents, pred=selected_element
                 )
+                score *= float(cur_score)
             elif "must_include" in target["required_contents"]:
                 required_contents = target["required_contents"]["must_include"]
                 assert isinstance(required_contents, list)
                 for content in required_contents:
                     content_or = content.split(" |OR| ")
-                    score *= any(
+                    cur_score = any(
                         [
                             StringEvaluator.must_include(
-                                ref=content, pred=selected_element
+                                ref=content,
+                                pred=selected_element,
+                                tokenize=False,
                             )
                             for content in content_or
                         ]
                     )
+                    score *= float(cur_score)
             else:
                 raise ValueError(
                     f"Unknown required_contents: {target['required_contents'].keys()}"
                 )
-
         return score
 
 
@@ -358,15 +406,14 @@ class EvaluatorComb:
         self,
         trajectory: Trajectory,
         config_file: Path | str,
-        page: Page | PseudoPage,
-        client: CDPSession,
+        page: Page | PseudoPage | None = None,
+        client: CDPSession | None = None,
     ) -> float:
 
         score = 1.0
         for evaluator in self.evaluators:
             cur_score = evaluator(trajectory, config_file, page, client)
             score *= cur_score
-
         return score
 
 
