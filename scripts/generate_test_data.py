9e21219538fb447b9f25942f75634c6093fb10b2
Tianyue Ou <tou3@jhu.edu>
2023-12-21 23:46:58 -0500

    fix pre-commit

--- scripts/generate_test_data.py
+++ scripts/generate_test_data.py
@@ -1,6 +1,23 @@
 """Replace the website placeholders with website domains from env_config
 Generate the test data"""
 import json
+import os
+
+# set the URLs of each website, we use the demo sites as an example
+os.environ["SHOPPING"] = "http:metis.lti.cs.cmu.edu:7770"
+os.environ["SHOPPING_ADMIN"] = "http:metis.lti.cs.cmu.edu:7780/admin"
+os.environ["REDDIT"] = "http:metis.lti.cs.cmu.edu:9999"
+os.environ["GITLAB"] = "http:metis.lti.cs.cmu.edu:8023"
+os.environ[
+    "MAP"
+] = "http://ec2-3-131-244-37.us-east-2.compute.amazonaws.com:3000"
+os.environ[
+    "WIKIPEDIA"
+] = "http:metis.lti.cs.cmu.edu:8888/wikipedia_en_all_maxi_2022-05/A/User:The_other_Kiwix_guy/Landing"
+os.environ[
+    "HOMEPAGE"
+] = "PASS"  # The home page is not currently hosted in the demo site
+print("Done setting up URLs")
 
 from browser_env.env_config import *
 
