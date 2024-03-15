39e0498da7d6494f719ab3f11f1adf1c7fa5067f
Simon Alford <simonalford42@gmail.com>
2024-03-15 11:22:11 -0400

    add setup_urls to generate test data

--- scripts/generate_test_data.py
+++ scripts/generate_test_data.py
@@ -2,11 +2,14 @@
 Generate the test data"""
 import json
 
+from utils import setup_urls
+setup_urls()
+
 from webarena.browser_env.env_config import *
 
 
 def main() -> None:
-    with open("config_files/test.raw.json", "r") as f:
+    with open("webarena/config_files/test.raw.json", "r") as f:
         raw = f.read()
     raw = raw.replace("__GITLAB__", GITLAB)
     raw = raw.replace("__REDDIT__", REDDIT)
@@ -14,12 +17,12 @@ def main() -> None:
     raw = raw.replace("__SHOPPING_ADMIN__", SHOPPING_ADMIN)
     raw = raw.replace("__WIKIPEDIA__", WIKIPEDIA)
     raw = raw.replace("__MAP__", MAP)
-    with open("config_files/test.json", "w") as f:
+    with open("webarena/config_files/test.json", "w") as f:
         f.write(raw)
     # split to multiple files
     data = json.loads(raw)
     for idx, item in enumerate(data):
-        with open(f"config_files/{idx}.json", "w") as f:
+        with open(f"webarena/config_files/{idx}.json", "w") as f:
             json.dump(item, f, indent=2)
 
 
